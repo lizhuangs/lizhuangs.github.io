a9683dc0bc1ee0ae945c165110dceb0d19b90b4a
tudou <le.z@foxmail.com>
2019-05-15 00:10:22 +0800

    Site updated at 2019-05-15 00:10:15

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,15 +1,14 @@
-<!--崩溃欺骗-->
 var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "/images/favicon.png");
-        document.title = '你不要我了 ╭(°A°`)╮ ';
+        document.title = '不要离开我好吗 ╭(°A°`)╮ ';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "/images/favicon.png");
-        document.title = '回来真好(ฅ>ω<*ฅ) ' + OriginTitle;
+        document.title = '你回来啦(ฅ>ω<*ฅ) ' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
